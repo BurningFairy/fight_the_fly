65faeda8c390eb91decd0c605570f67de2d9355d
Admin <irina.karner@outlook.at>
2025-09-30 19:11:02 +0200

    added movement to apple

--- character.py
+++ character.py
@@ -2,9 +2,33 @@ import pygame
 
 class Apple: 
     def __init__(apple, image, x, y):
-        apple.image = pygame.image.load(image)
+        apple.width = 100
+        apple.height = 100
+
+        unscaled_image = pygame.image.load(image)
+        apple.image = pygame.transform.smoothscale(unscaled_image, (apple.width, apple.height))
         apple.x = x
         apple.y = y
+        apple.movespeed = 5
 
     def draw_apple(apple, window):
         window.blit(apple.image, (apple.x, apple.y))
+
+    def handle_apple_movement(apple, keys):
+        if keys[pygame.K_UP]:
+            apple.y -= apple.movespeed
+        if keys[pygame.K_DOWN]:
+            apple.y += apple.movespeed
+        if keys[pygame.K_LEFT]:
+            apple.x -= apple.movespeed
+        if keys[pygame.K_RIGHT]:
+            apple.x += apple.movespeed
+
+        if keys[pygame.K_w]:
+            apple.y -= apple.movespeed
+        if keys[pygame.K_s]:
+            apple.y += apple.movespeed
+        if keys[pygame.K_a]:
+            apple.x -= apple.movespeed
+        if keys[pygame.K_d]:
+            apple.x += apple.movespeed
--- main.py
+++ main.py
@@ -10,13 +10,14 @@ WINDOW = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Fight The Fly")
 
 apple = Apple("assets/Apple_back.png", 100, 100)
+BG = pygame.image.load("assets/BG_Test.png")
 
 class colors:
     white = (255, 255, 255)
     black = (0, 0, 0)
 
-def draw_window():
-    WINDOW.fill(colors.white)
+def draw_window(background):
+    WINDOW = pygame.Surface.blit(background)
     apple.draw_apple(WINDOW)
     pygame.display.update()
 
@@ -28,9 +29,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
 
-        draw_window()
+        keys = pygame.key.get_pressed()
+        apple.handle_apple_movement(keys)
+
+        draw_window(BG)
 
 if __name__ == "__main__":
     main()
