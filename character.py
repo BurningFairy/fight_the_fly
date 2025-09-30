c47db9f2d42cd1736559c9b7b35195afe905d048
Irina <irina.karner@outlook.at>
2025-09-30 21:46:36 +0200

    added movement restriction to apple

--- character.py
+++ character.py
@@ -1,9 +1,9 @@
 import pygame
 
 class Apple: 
-    def __init__(apple, image, x, y):
-        apple.width = 100
-        apple.height = 100
+    def __init__(apple, image, x, y, height = 100, width = 100):
+        apple.width = height
+        apple.height = width
 
         unscaled_image = pygame.image.load(image)
         apple.image = pygame.transform.smoothscale(unscaled_image, (apple.width, apple.height))
@@ -12,23 +12,24 @@ class Apple:
         apple.movespeed = 5
 
     def draw_apple(apple, window):
-        window.blit(apple.image, (apple.x, apple.y))
+        window.blit(apple.image, (apple.x, apple.y)) # draws the char onto the WINDOW surface
 
-    def handle_apple_movement(apple, keys):
-        if keys[pygame.K_UP]:
+    def handle_apple_movement(apple, keys, width, height):
+
+        # move up + restriction
+        if apple.y - apple.movespeed > 0 and (keys[pygame.K_UP] or keys[pygame.K_w]):
             apple.y -= apple.movespeed
-        if keys[pygame.K_DOWN]:
+
+        # move down + restriction
+        if apple.y + apple.movespeed + apple.height < height and (keys[pygame.K_DOWN] or keys[pygame.K_s]):
             apple.y += apple.movespeed
-        if keys[pygame.K_LEFT]:
+
+        # move left + restriction
+        if apple.x - apple.movespeed > 0 and (keys[pygame.K_LEFT] or keys[pygame.K_a]):
             apple.x -= apple.movespeed
-        if keys[pygame.K_RIGHT]:
+
+        # move right + restriction
+        if apple.x + apple.movespeed + apple.width < width and (keys[pygame.K_RIGHT] or keys[pygame.K_d]):
             apple.x += apple.movespeed
 
-        if keys[pygame.K_w]:
-            apple.y -= apple.movespeed
-        if keys[pygame.K_s]:
-            apple.y += apple.movespeed
-        if keys[pygame.K_a]:
-            apple.x -= apple.movespeed
-        if keys[pygame.K_d]:
-            apple.x += apple.movespeed
+        
--- main.py
+++ main.py
@@ -9,16 +9,19 @@ WINDOW = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
 pygame.display.set_caption("Fight The Fly")
 
+# creates an Object from the "Apple" Class 
 apple = Apple("assets/Apple_back.png", 100, 100)
-BG = pygame.image.load("assets/BG_Test.png")
+
+# sets path and scales the BG-image to the WINDOW size
+BG_IMAGE = pygame.transform.smoothscale(pygame.image.load("assets/BG_Test.png"), (WIN_WIDTH, WIN_HEIGHT))
 
 class colors:
     white = (255, 255, 255)
     black = (0, 0, 0)
 
-def draw_window(background):
-    WINDOW = pygame.Surface.blit(background)
-    apple.draw_apple(WINDOW)
+def draw_window():
+    WINDOW.blit(BG_IMAGE, (0,0)) # inserts the BG_Image to the WINDOW
+    apple.draw_apple(WINDOW) # calls the draw function from character.py
     pygame.display.update()
 
 def main():
@@ -30,10 +33,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        keys = pygame.key.get_pressed()
-        apple.handle_apple_movement(keys)
+        apple.handle_apple_movement(pygame.key.get_pressed(), WIN_WIDTH, WIN_HEIGHT)
 
-        draw_window(BG)
+        draw_window()
 
 if __name__ == "__main__":
     main()
