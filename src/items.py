9abaa8c63164a05a9be47572b517aadeaa0a99ee
DavidRin <86775318+DavidRin@users.noreply.github.com>
2025-11-19 11:14:01 +0100

    changes itemsclass

---
+++ src/items.py
@@ -0,0 +1,27 @@
+"""This file is for Items ."""
+import pygame
+
+class Items:
+     """Parent Class for items."""
+     # 1 def__init__(self,image,window,positon )
+     # 2 displaymethod for drawing item in slot
+
+class Weapon(Items):
+     """Class for the basic functions for weapons."""
+     pass
+
+class Flyswater(Weapon):
+     """Class for the functions of Flyswater ."""
+     pass
+
+class Cocktailpick(Weapon):
+     """Class for the basic functions for Accessories ."""
+     pass
+
+class Bugspray(Weapon):
+     """Class for the basic functions for Accessories ."""
+     pass
+
+class Accessories(Items):
+     """Class for the basic functions for Accessories ."""
+     pass
---
+++ src/shop.py
@@ -0,0 +1,8 @@
+"""This file is for the shop, where items can be bought ."""
+import pygame
+
+
+class Shop:
+     """Shop for buying items."""
+
+     
