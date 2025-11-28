2b75cc5d68e393c46f14e0d0180adcdbbbc7b266
Admin <irina.karner@outlook.at>
2025-11-28 07:53:51 +0100

    changed folder structure and added screens folder

---
+++ src/screens/gameWindow.py
@@ -0,0 +1,15 @@
+"""This file contains the functions that draws the game window"""
+
+import pygame
+
+from characters.character import Apple
+from characters.enemies import Fly
+import settings
+
+
+def draw_window(apple):
+    """Draw the charcter and the background on the screen."""
+    settings.WINDOW.blit(settings.BG_IMAGE, (0, 0))  # inserts the BG_Image to the WINDOW
+    apple.draw_apple(settings.WINDOW)  # calls the draw function from character.py
+    #enemy.draw_enemy(settings.WINDOW) # calls the draw function from enemies.py
+    pygame.display.update()
