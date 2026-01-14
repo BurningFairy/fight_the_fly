af1dff0ebd2802445c9f56aedb83bff44316584e
Jennifer Neumaier <jennifer.neumaier@ms.bulme.at>
2026-01-14 13:25:19 +0100

    enemies spawn randomly

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 """This file includes the main function."""
 import pygame
+import random
 
 from characters.character import Apple
 from characters.enemies import Fly
@@ -16,14 +17,27 @@ from shop import items
 # creates an Object from the "Apple" Class
 apple = Apple("assets/char_apple/Apple_back_cropped.png", 400, 400)
 
-#last_spawned=0
-#spawn_interval =30000
-flies=[Fly("assets/char_fly/Fly.png", 100, 100),
-       Fly("assets/char_fly/Fly.png", 200, 200),
-       Fly("assets/char_fly/Fly.png", 300, 500),
-       Fly("assets/char_fly/Fly.png", 300, 600),
-       Fly("assets/char_fly/Fly.png", 400, 300),
-       Fly("assets/char_fly/Fly.png", 500, 300)]
+# creates random coordinates, that will be used to set the spawn-point of the enemies
+ax= random.randint(0,600)
+ay= random.randint(0,300)
+bx= random.randint(600,1200)
+by= random.randint(0,300)
+cx= random.randint(0,600)
+cy= random.randint(300,700)
+dx= random.randint(600,1200)
+dy= random.randint(300,700)
+rx= random.randint(0,1200)
+ry= random.randint(0,700)
+
+
+def enemy_create(x, y):
+    return Fly("assets/char_fly/Fly.png", x, y)
+
+flies=[enemy_create(ax, ay),
+    enemy_create(bx, by),
+    enemy_create(cx, cy),
+    enemy_create(dx, dy),
+    enemy_create(rx, ry)]
 
 def update_game():
     global flies
@@ -41,6 +55,23 @@ def update_game():
     
     flies = alive_flies
 
+    ax= random.randint(0,600)
+    ay= random.randint(0,300)
+    bx= random.randint(600,1200)
+    by= random.randint(0,300)
+    cx= random.randint(0,600)
+    cy= random.randint(300,700)
+    dx= random.randint(600,1200)
+    dy= random.randint(300,700)
+    rx= random.randint(0,1200)
+    ry= random.randint(0,700)
+
+    if flies == []:
+        flies= [enemy_create(ax, ay),
+    enemy_create(bx, by),
+    enemy_create(cx, cy),
+    enemy_create(dx, dy),
+    enemy_create(rx, ry)]
     
 def main():
     """Define the main function of the game."""
